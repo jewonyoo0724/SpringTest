cbb86b37ed32288ba2c9abe45dd74b683a4b98ff
Jewon Yoo <skywalker0724@gmail.com>
2023-07-05 20:08:35 +0900

    - hello edited (@Controller)

--- src/main/java/com/alpaca/spring/test/hello/TestController.java
+++ src/main/java/com/alpaca/spring/test/hello/TestController.java
@@ -1,8 +1,10 @@
 package com.alpaca.spring.test.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class TestController {
 
 	@ResponseBody
